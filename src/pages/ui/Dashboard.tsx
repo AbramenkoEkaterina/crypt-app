d80001fec05267a0a39131c1f3c7bf795aa90583
Abramenko Ekaterina <katiatgn@yandex.ru>
2026-05-11 09:26:41 +0300

    компонент loader добавила

--- src/pages/ui/Dashboard.tsx
+++ src/pages/ui/Dashboard.tsx
@@ -5,5 +5,8 @@ export const Doshboard = () => {
   const { data, isLoading, error } = useGetAssetsQuery();
   if (isLoading) return <div> Loading ...</div>;
   if (error) return <div> Error...</div>;
+  if (!data || data.length === 0) {
+    return <div> No coins found</div>;
+  }
   return <>{data && <MarketTable coins={data} />}</>;
 };
